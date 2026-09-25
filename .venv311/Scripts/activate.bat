15103fdc922824a8dfa266c705b4c11b311a9ed7
Kazi Aynun nahin <nahian1269@gmail.com>
2026-09-25 23:30:40 +0600

    main

--- .venv311/Scripts/activate.bat
+++ .venv311/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\FinalFYDP\YOLOv12-for-Smart-Agriculture-Crop-Disease-Detection-main\.venv311
+set VIRTUAL_ENV=D:\NeuroAgro\YOLOv12-for-Smart-Agriculture-Crop-Disease-Detection-main\.venv311
 
 if not defined PROMPT set PROMPT=$P$G
 
